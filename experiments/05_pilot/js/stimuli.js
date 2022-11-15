53671076591cb8e95a944742601e2d1424964cee
Tony Velasquez <tonyvelasquez@DN0a1ea546.SUNet>
2022-11-15 12:37:49 -0800

    05_pilot created

---
+++ experiments/05_pilot/js/stimuli.js
@@ -0,0 +1,12 @@
+var all_stims =
+
+  [{
+    'item': 'gamenight_agn_ub',
+    'context': 'Rachel and Neil are assistants at a senior center, and are in charge of hosting a game night for their residents. For the game they plan on playing, they need NUMLIMIT residents or less. Rachel has heard from all the residents about whether they\'ll attend. Neil asks her, "How many people said they would come?"',
+    'answer': 'Rachel replies, "NUMBERUTT."'
+  },
+  {
+    'item': 'gamenight_agn_lb',
+    'context': 'Rachel and Neil are assistants at a senior center, and are in charge of hosting a game night for their residents. For the game they plan on playing, they need NUMLIMIT residents or more. Rachel has heard from all the residents about whether they\'ll attend. Neil asks her, "How many people said they would come?"',
+    'answer': 'Rachel replies, "NUMBERUTT."'
+  }]
